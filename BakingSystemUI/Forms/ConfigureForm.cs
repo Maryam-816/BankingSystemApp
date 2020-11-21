5fcc1f69f2766d8c1817d56e0fa1e9323962b645
Maryam-816 <rustamova.maryam2009@gmail.com>
2020-11-21 21:14:45 +0400

    ConfigureForm and CardsForm added

---
+++ BakingSystemUI/Forms/ConfigureForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BakingSystemUI.Forms
+{
+    public partial class ConfigureForm : Form
+    {
+        public ConfigureForm()
+        {
+            InitializeComponent();
+        }
+
+        private void grbx_user_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
