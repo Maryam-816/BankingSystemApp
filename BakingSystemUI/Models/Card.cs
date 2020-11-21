9ad6198fe19ee3d8662156b336d9b28efd74e711
Maryam-816 <rustamova.maryam2009@gmail.com>
2020-11-21 22:51:23 +0400

    Models added

---
+++ BakingSystemUI/Models/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BakingSystemUI.Roles;
+
+namespace BakingSystemUI.Models
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public BankName Bank { get; set; }
+        public DurationType Duration { get; set; }
+        public CardType CardType { get; set; }
+        public string CardNumber { get; set; }
+        public short CVC { get; set; }
+        public DateTime ExpiredDate { get; set; }
+
+        public string OwnerName { get; set; }
+        public int OwnerId { get; set; }
+    }
+}
