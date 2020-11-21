cc23cfa260a3a414d1555f35e62cc8bfceab1b6c
Maryam-816 <rustamova.maryam2009@gmail.com>
2020-11-21 21:11:14 +0400

    RegLogControl changed

--- BakingSystemUI/Controls/RegLogControl.cs
+++ BakingSystemUI/Controls/RegLogControl.cs
@@ -16,5 +16,10 @@ namespace BakingSystemUI.Controls
         {
             InitializeComponent();
         }
+
+        private void btn_submit_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
