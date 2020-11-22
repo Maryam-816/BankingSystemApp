dc2af8d6c5d1438f056ee0779f66b9ec2b3cd66e
Maryam-816 <rustamova.maryam2009@gmail.com>
2020-11-22 11:20:54 +0400

    Session and Validator logics added

--- BakingSystemUI/Forms/LogForm.cs
+++ BakingSystemUI/Forms/LogForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BakingSystemUI.Models;
+using BakingSystemUI.Roles;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -22,13 +24,26 @@ namespace BakingSystemUI.Forms
 
         private void btn_login_clicked(object sender, EventArgs e)
         {
-           
+              //get data
+             //validate data
+            //check data - database
+           //go to new form or XXX
         }
 
         private void btn_register_clicked(object sender, EventArgs e)
         {
-           
+            //get data
+            string
+                 email = txbx_registerEmail.Text,
+                 password = txbx_registerPassword.Text;
         }
+        //validate data
+
+        //register
+        User user = new User { Email = email, Password = password, UserType = UserType.User };
+        MessageBox.Show("You successfully registered!");
+
+       
 
         private void LogForm_Load(object sender, EventArgs e)
         {
