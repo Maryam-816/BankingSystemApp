a4fab353d0ec0808dac20ddd9005b73fd80b1418
Maryam-816 <rustamova.maryam2009@gmail.com>
2020-11-28 11:43:38 +0400

    LogForm updated

--- BakingSystemUI/Forms/LogForm.Designer.cs
+++ BakingSystemUI/Forms/LogForm.Designer.cs
@@ -28,146 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.grbx_regisrter = new System.Windows.Forms.GroupBox();
-            this.txbx_registerPassword = new System.Windows.Forms.TextBox();
-            this.lbl_registerPassword = new System.Windows.Forms.Label();
-            this.txbx_registerEmail = new System.Windows.Forms.TextBox();
-            this.lbl_registerEmail = new System.Windows.Forms.Label();
-            this.btn_registerSubmit = new System.Windows.Forms.Button();
-            this.grbx_login = new System.Windows.Forms.GroupBox();
-            this.txbx_loginPassword = new System.Windows.Forms.TextBox();
-            this.lbl_loginPassword = new System.Windows.Forms.Label();
-            this.txbx_loginEmail = new System.Windows.Forms.TextBox();
-            this.lbl_loginEmail = new System.Windows.Forms.Label();
-            this.btn_loginSubmit = new System.Windows.Forms.Button();
-            this.grbx_regisrter.SuspendLayout();
-            this.grbx_login.SuspendLayout();
+            this.grbx_reg = new System.Windows.Forms.GroupBox();
+            this.grbx_log = new System.Windows.Forms.GroupBox();
+            this.regLogControl1 = new BakingSystemUI.Controls.RegLogControl();
+            this.regLogControl2 = new BakingSystemUI.Controls.RegLogControl();
+            this.grbx_reg.SuspendLayout();
+            this.grbx_log.SuspendLayout();
             this.SuspendLayout();
             // 
-            // grbx_regisrter
+            // grbx_reg
             // 
-            this.grbx_regisrter.Controls.Add(this.txbx_registerPassword);
-            this.grbx_regisrter.Controls.Add(this.lbl_registerPassword);
-            this.grbx_regisrter.Controls.Add(this.txbx_registerEmail);
-            this.grbx_regisrter.Controls.Add(this.lbl_registerEmail);
-            this.grbx_regisrter.Controls.Add(this.btn_registerSubmit);
-            this.grbx_regisrter.Location = new System.Drawing.Point(12, 12);
-            this.grbx_regisrter.Name = "grbx_regisrter";
-            this.grbx_regisrter.Size = new System.Drawing.Size(194, 187);
-            this.grbx_regisrter.TabIndex = 0;
-            this.grbx_regisrter.TabStop = false;
-            this.grbx_regisrter.Text = "Register";
+            this.grbx_reg.Controls.Add(this.regLogControl1);
+            this.grbx_reg.Location = new System.Drawing.Point(28, 12);
+            this.grbx_reg.Name = "grbx_reg";
+            this.grbx_reg.Size = new System.Drawing.Size(226, 223);
+            this.grbx_reg.TabIndex = 0;
+            this.grbx_reg.TabStop = false;
+            this.grbx_reg.Text = "Register";
             // 
-            // txbx_registerPassword
+            // grbx_log
             // 
-            this.txbx_registerPassword.Location = new System.Drawing.Point(13, 107);
-            this.txbx_registerPassword.Name = "txbx_registerPassword";
-            this.txbx_registerPassword.Size = new System.Drawing.Size(174, 20);
-            this.txbx_registerPassword.TabIndex = 9;
+            this.grbx_log.Controls.Add(this.regLogControl2);
+            this.grbx_log.Location = new System.Drawing.Point(313, 12);
+            this.grbx_log.Name = "grbx_log";
+            this.grbx_log.Size = new System.Drawing.Size(226, 223);
+            this.grbx_log.TabIndex = 1;
+            this.grbx_log.TabStop = false;
+            this.grbx_log.Text = "Login";
             // 
-            // lbl_registerPassword
+            // regLogControl1
             // 
-            this.lbl_registerPassword.AutoSize = true;
-            this.lbl_registerPassword.Location = new System.Drawing.Point(10, 82);
-            this.lbl_registerPassword.Name = "lbl_registerPassword";
-            this.lbl_registerPassword.Size = new System.Drawing.Size(56, 13);
-            this.lbl_registerPassword.TabIndex = 8;
-            this.lbl_registerPassword.Text = "Password:";
+            this.regLogControl1.Location = new System.Drawing.Point(21, 46);
+            this.regLogControl1.Name = "regLogControl1";
+            this.regLogControl1.Size = new System.Drawing.Size(179, 140);
+            this.regLogControl1.TabIndex = 0;
             // 
-            // txbx_registerEmail
+            // regLogControl2
             // 
-            this.txbx_registerEmail.Location = new System.Drawing.Point(13, 53);
-            this.txbx_registerEmail.Name = "txbx_registerEmail";
-            this.txbx_registerEmail.Size = new System.Drawing.Size(174, 20);
-            this.txbx_registerEmail.TabIndex = 7;
-            // 
-            // lbl_registerEmail
-            // 
-            this.lbl_registerEmail.AutoSize = true;
-            this.lbl_registerEmail.Location = new System.Drawing.Point(10, 28);
-            this.lbl_registerEmail.Name = "lbl_registerEmail";
-            this.lbl_registerEmail.Size = new System.Drawing.Size(35, 13);
-            this.lbl_registerEmail.TabIndex = 6;
-            this.lbl_registerEmail.Text = "Email:";
-            // 
-            // btn_registerSubmit
-            // 
-            this.btn_registerSubmit.Location = new System.Drawing.Point(58, 142);
-            this.btn_registerSubmit.Name = "btn_registerSubmit";
-            this.btn_registerSubmit.Size = new System.Drawing.Size(75, 23);
-            this.btn_registerSubmit.TabIndex = 5;
-            this.btn_registerSubmit.Text = "Register";
-            this.btn_registerSubmit.UseVisualStyleBackColor = true;
-            // 
-            // grbx_login
-            // 
-            this.grbx_login.Controls.Add(this.txbx_loginPassword);
-            this.grbx_login.Controls.Add(this.lbl_loginPassword);
-            this.grbx_login.Controls.Add(this.txbx_loginEmail);
-            this.grbx_login.Controls.Add(this.lbl_loginEmail);
-            this.grbx_login.Controls.Add(this.btn_loginSubmit);
-            this.grbx_login.Location = new System.Drawing.Point(248, 12);
-            this.grbx_login.Name = "grbx_login";
-            this.grbx_login.Size = new System.Drawing.Size(194, 187);
-            this.grbx_login.TabIndex = 1;
-            this.grbx_login.TabStop = false;
-            this.grbx_login.Text = "Login";
-            // 
-            // txbx_loginPassword
-            // 
-            this.txbx_loginPassword.Location = new System.Drawing.Point(12, 107);
-            this.txbx_loginPassword.Name = "txbx_loginPassword";
-            this.txbx_loginPassword.Size = new System.Drawing.Size(174, 20);
-            this.txbx_loginPassword.TabIndex = 9;
-            // 
-            // lbl_loginPassword
-            // 
-            this.lbl_loginPassword.AutoSize = true;
-            this.lbl_loginPassword.Location = new System.Drawing.Point(9, 82);
-            this.lbl_loginPassword.Name = "lbl_loginPassword";
-            this.lbl_loginPassword.Size = new System.Drawing.Size(56, 13);
-            this.lbl_loginPassword.TabIndex = 8;
-            this.lbl_loginPassword.Text = "Password:";
-            // 
-            // txbx_loginEmail
-            // 
-            this.txbx_loginEmail.Location = new System.Drawing.Point(12, 53);
-            this.txbx_loginEmail.Name = "txbx_loginEmail";
-            this.txbx_loginEmail.Size = new System.Drawing.Size(174, 20);
-            this.txbx_loginEmail.TabIndex = 7;
-            // 
-            // lbl_loginEmail
-            // 
-            this.lbl_loginEmail.AutoSize = true;
-            this.lbl_loginEmail.Location = new System.Drawing.Point(9, 28);
-            this.lbl_loginEmail.Name = "lbl_loginEmail";
-            this.lbl_loginEmail.Size = new System.Drawing.Size(35, 13);
-            this.lbl_loginEmail.TabIndex = 6;
-            this.lbl_loginEmail.Text = "Email:";
-            // 
-            // btn_loginSubmit
-            // 
-            this.btn_loginSubmit.Location = new System.Drawing.Point(57, 142);
-            this.btn_loginSubmit.Name = "btn_loginSubmit";
-            this.btn_loginSubmit.Size = new System.Drawing.Size(75, 23);
-            this.btn_loginSubmit.TabIndex = 5;
-            this.btn_loginSubmit.Text = "Login";
-            this.btn_loginSubmit.UseVisualStyleBackColor = true;
+            this.regLogControl2.Location = new System.Drawing.Point(21, 46);
+            this.regLogControl2.Name = "regLogControl2";
+            this.regLogControl2.Size = new System.Drawing.Size(179, 140);
+            this.regLogControl2.TabIndex = 0;
             // 
             // LogForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(481, 226);
-            this.Controls.Add(this.grbx_login);
-            this.Controls.Add(this.grbx_regisrter);
+            this.ClientSize = new System.Drawing.Size(571, 254);
+            this.Controls.Add(this.grbx_log);
+            this.Controls.Add(this.grbx_reg);
             this.Name = "LogForm";
-            this.Text = "BankingSystem";
-            this.Load += new System.EventHandler(this.LogForm_Load);
-            this.grbx_regisrter.ResumeLayout(false);
-            this.grbx_regisrter.PerformLayout();
-            this.grbx_login.ResumeLayout(false);
-            this.grbx_login.PerformLayout();
+            this.grbx_reg.ResumeLayout(false);
+            this.grbx_log.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -186,5 +96,9 @@
         private System.Windows.Forms.TextBox txbx_loginEmail;
         private System.Windows.Forms.Label lbl_loginEmail;
         private System.Windows.Forms.Button btn_loginSubmit;
+        private System.Windows.Forms.GroupBox grbx_reg;
+        private Controls.RegLogControl regLogControl1;
+        private System.Windows.Forms.GroupBox grbx_log;
+        private Controls.RegLogControl regLogControl2;
     }
 }
--- BakingSystemUI/Forms/LogForm.cs
+++ BakingSystemUI/Forms/LogForm.cs
@@ -14,19 +14,36 @@ namespace BakingSystemUI.Forms
         {
             InitializeComponent();
             //+
-            btn_registerSubmit.Click += btn_register_clicked;
-            btn_loginSubmit.Click += btn_login_clicked;
+            regLogControl1.btn_submit.Text = "Register";
+            regLogControl2.btn_submit.Text = "Login";
+            regLogControl1.btn_submit.Click += btn_register_clicked;
+            regLogControl2.btn_submit.Click += btn_login_clicked;
             dbContext = new DbContext();
         }
-        
+
+        private void btn_register_clicked(object sender, EventArgs e)
+        {
+            //get data
+            string
+                 email = regLogControl1.txbx_email.Text,
+                 password = regLogControl1.txbx_password.Text;
+
+            if (dbContext.Users.FindItem(u => u.Email == email) == null)
+            {
+                User user = new User { Id = Identificator<User>.GetId(), Email = email, Password = password, UserType = UserType.User };
+                dbContext.Users.Add(user);
+                MessageBox.Show("You successfully registered!");
+            }
+        }
+
         private void btn_login_clicked(object sender, EventArgs e)
         {
             //get data
             string
-              email = txbx_loginEmail.Text,//
-              password = txbx_loginPassword.Text;//
-            //validate data
-            //check data - database
+              email = regLogControl2.txbx_email.Text,
+              password = regLogControl2.txbx_password.Text;
+            //    //validate data
+            //    //check data - database
             User currentUser = dbContext.Users.FindItem(u => u.Email == email);
             if (currentUser != null)
             {
@@ -34,47 +51,26 @@ namespace BakingSystemUI.Forms
                 Session.LogForm = this;
                 if (currentUser.UserType == UserType.User)
                 {
-                    
+
                     //MessageBox.Show("You successfully logged in!");
                     new MainForm().Show();
                     Hide();
                 }
 
-                else if(currentUser.UserType == UserType.Admin)
+                else if (currentUser.UserType == UserType.Admin)
                 {
                     new MainForm().Show();
                     Hide();
                 }
             }
-                
-            }
             else
             {
                 MessageBox.Show("Your email or password is wrong!");
             }
-            //go to new form or XXX
         }
 
-        private void btn_register_clicked(object sender, EventArgs e)
-        {
-            //get data
-            string
-                 email = txbx_registerEmail.Text,//
-                 password = txbx_registerPassword.Text;//
-
-            //register
-            
-            if(dbContext.Users.FindItem(u => u.Email == email) == null)
-            {
-                User user = new User { Id = Identificator<User>.GetId(), Email = email, Password = password, UserType = UserType.User };
-                dbContext.Users.Add(user);
-                MessageBox.Show("You successfully registered!");
-            }
-           
-        }
-        private void LogForm_Load(object sender, EventArgs e)
-        {
-
-        }
+        //    //go to new form or XXX
     }
+
 }
+
