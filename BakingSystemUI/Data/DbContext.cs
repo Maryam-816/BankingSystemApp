85324e9a4a265defac16d105794dd259141bbec1
Maryam-816 <rustamova.maryam2009@gmail.com>
2020-12-04 17:53:31 +0400

    Data's files updated

--- BakingSystemUI/Data/DbContext.cs
+++ BakingSystemUI/Data/DbContext.cs
@@ -1,4 +1,5 @@
 ﻿using BakingSystemUI.Core;
+using BakingSystemUI.Managers;
 using BakingSystemUI.Models;
 using System;
 using System.Collections.Generic;
@@ -6,25 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace BakingSystemUI.Data
 {
     public class DbContext
     {
-        public  DbSet<User> Users;
-        public  DbSet<Card> Cards;
+        public DbSet<User> Users;
+        public DbSet<Card> Cards;
 
         public DbContext()
         {
             Users = new DbSet<User>();
             Cards = new DbSet<Card>();
+            AddDefaultUsers();
+            AddGefaultCards();
         }
+
+        private List<User> defaultUsers = new List<User>
+        {
+            new User { Id = Identificator<User>.GetId(), Email = "maryam@gmail.com", Password = "123456", UserType = Roles.UserType.User },
+            new User { Id = Identificator<User>.GetId(), Email = "admin@gmail.com", Password = "admin1", UserType = Roles.UserType.Admin},
+            new User { Id = Identificator<User>.GetId(), Name = "Julia", Surname = "Rustamova", Age = 38, Email = "julia@gmail.com", Password = "J08101982A", UserType = Roles.UserType.User}
+        };
 
         public void AddDefaultUsers()
         {
-            Users.Add(new User { Id = Identificator<User>.GetId(), Email = "maryam@gmail.com", Password = "12345", UserType = Roles.UserType.Moderator });
-            Users.Add(new User { Id = Identificator<User>.GetId(), Email = "admin@gmail.com", Password = "admin1", UserType = Roles.UserType.Admin });
-            Users.Add(new User { Id = Identificator<User>.GetId(), Name = "Julia", Surname = "Rustamova", Age = 38, Email = "julia@gmail.com", Password = "J08101982A", UserType = Roles.UserType.User });
+            Users.Add(defaultUsers[0]);
+            Users.Add(defaultUsers[1]);
+            Users.Add(defaultUsers[2]);
+        }
 
+        private void AddGefaultCards()
+        {
+            Cards.Add(new CardManager("Kapital Bank", "5", "Debet", defaultUsers[2]).GetCard());
+            Cards.Add(new CardManager("BTB Bank", "1", "Scolarship", defaultUsers[0]).GetCard());
         }
     }
 }
