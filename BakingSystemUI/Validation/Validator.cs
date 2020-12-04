4cf6d2c704df76682acd12c6672df6278f4fd6b5
Maryam-816 <rustamova.maryam2009@gmail.com>
2020-12-04 17:54:34 +0400

    Validator logic added

--- BakingSystemUI/Validation/Validator.cs
+++ BakingSystemUI/Validation/Validator.cs
@@ -6,49 +6,56 @@ using System.Threading.Tasks;
 
 namespace BakingSystemUI.Validation
 {
-    public class Validator
+    public static class Validator
     {
-        //public static void Validate(params string[] txbxs)
-        //{
-        //    foreach (string txbx in txbxs)
-        //    {
-        //        if (string.IsNullOrEmpty(txbx))
-        //        {
-        //            throw new ArgumentNullException("Input is empty. Please fill out them all!");
+        public static void Validate(params string[] txbxs)
+        {
+            foreach (string txbx in txbxs)
+            {
+                if (string.IsNullOrEmpty(txbx))
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+        }
 
-        //        }
-        //    }
-        //}
+        public static sbyte IsAgeValueValid(string age, sbyte limit)
+        {
+            if (string.IsNullOrEmpty(age))
+            {
+                throw new ArgumentNullException("Age field is empty!");
+            }
+            if (!sbyte.TryParse(age, out sbyte result))
+            {
+                throw new FormatException();
+            }
+            if (sbyte.Parse(age) < limit)
+            {
+                throw new AgeValueValidException();
+            }
+            return result;
+        }
 
-        //public static sbyte IsAgeValueValid(string age, sbyte limit)
-        //{
-        //    if (string.IsNullOrEmpty(age))
-        //        throw new ArgumentNullException("Age field is empty!");
-        //    if (!sbyte.TryParse(age, out sbyte result))
-        //        throw new FormatException("Age value is not valid!");
-        //    if (sbyte.Parse(age) < limit)
-        //        throw new ValidAgeValueException("Age value is less than zero! Please enter the valid value!");
+        public static void IsEmailValid(string email)
+        {
+            if (string.IsNullOrEmpty(email))
+            {
+                throw new ArgumentNullException("Email field is empty!");
+            }
+                
+            if (!email.Contains("@"))
+            {
+                throw new InvalidEmailException("Email must contains at(@)");
+            }
+        }
 
-        //    return result;
+        public static void IsPasswordValid(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+                throw new ArgumentNullException();
+            if (password.Length < 4)
+                throw new InvalidPasswordException("Length of password must contains 4 or more characters");
 
-        //}
-
-        //public static void IsEmailValid(string email)
-        //{
-        //    if (string.IsNullOrEmpty(email))
-        //        throw new ArgumentNullException("Email is empty!");
-        //    if (!email.Contains("@"))
-        //        throw new InvalidEmailException();
-
-        //}
-
-        //public static void IsPasswordValid(string password)
-        //{
-        //    if (string.IsNullOrEmpty(password))
-        //        throw new ArgumentNullException();
-        //    if (password.Length < 4)
-        //        throw new InvalidPasswordException("Password is not valid. Length must contains 4 or more");
-
-        //}
+        }
     }
 }
